0795fd2757e89ed48b12c498dd6d2339255d1bb8
MAK8405 <mahdiahmadi2005kf@gmail.com>
2025-11-21 09:27:20 +0330

    Remove comments

--- Lectures/04 Your First Python Project/03 Number Guesser Game Project/03.1 Solution A/number_guesser.py
+++ Lectures/04 Your First Python Project/03 Number Guesser Game Project/03.1 Solution A/number_guesser.py
@@ -1,18 +1,8 @@
-# Generate random number:
 import random
-
-# 👉 random is a module
-# A module in Python = a file that contains many functions, classes, and variables.
 
 
 def validate_input(input_num):
-    # 1 < input_num < 100
-    # > 100
-    # < 1
-    # q
-    # non digit
     if not input_num.isdigit():
-        # with isdigit we can if a string is a number or not
         print("Invalid input. Please enter a number.")
         return False
 
@@ -25,27 +15,17 @@ def validate_input(input_num):
 
 
 def start_game():
-    # int(random.random() * 100 + 1)
-    # random.random is a uniform distribution, every number between 0 to 1 have a same luch for generate.
-    # Better choice than the up one than 2 previous line
     rand_num = random.randint(1, 100)
     score = 100
 
-    # Indefinite loop -> we don't know till where it will continue, we will continue till it is finished.
     while True:
         input_num = input("Enter your guess between 1 and 100:")
-        # The output of "input" is always string and we should convert it to integer"
-        # plus the user should enter a number, not a character
-        # before convert it to string, we should validate the input
-        # non digit -> not a number
 
         if input_num == "q":
             print("Goodbye!")
             break
-        # -> When we reach the proper condition we will break the lopp
 
         if not validate_input(input_num):
-            # q for quit
             continue
 
         input_num = int(input_num)
@@ -59,7 +39,6 @@ def start_game():
             else:
                 print("Goodbye!")
                 break
-            # -> When we reach the proper condition we will break the lopp
 
         elif input_num > rand_num:
             print("You guessed too high!")
